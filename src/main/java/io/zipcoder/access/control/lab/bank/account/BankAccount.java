46ab5593504a1a99db54a3e4cab8b053bd679ac2
Santos <E126768@NAEAST.AD.JPMORGANCHASE.COM>
2017-06-21 15:45:58 -0400

    wrote first test

--- src/main/java/io/zipcoder/access/control/lab/bank/account/BankAccount.java
+++ src/main/java/io/zipcoder/access/control/lab/bank/account/BankAccount.java
@@ -1,5 +1,31 @@
 package io.zipcoder.access.control.lab.bank.account;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BankAccount {
+	
+	//Fields
+	
+	private enum AcctType {CHECKING, SAVINGS, INVESTMENT};
+	private String acctNum;
+	private double balance;
+	private String acctHolder;
+	private double interestRate;
+	public enum STATUS {OPEN, CLOSED, OFAC_FREEZE};
+	private enum OD_Prevention {ENABLED, DISABLED, AUTO_ACCT_TRANSFER};
+	private ArrayList<String> history = new ArrayList<> (Arrays.asList(transactions));
+	
+	//Methods
+	
+	public boolean setStatus(STATUS acctStatus){
+		//set status to open
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 }
